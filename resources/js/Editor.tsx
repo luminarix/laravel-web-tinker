39ce29f6e41efb9fe0642997831a1eb12ccaf8fd
Gere Lajos <streetnoizenova@gmail.com>
2024-05-26 17:12:27 +0200

    refactor: extract Header component

--- resources/js/Editor.tsx
+++ resources/js/Editor.tsx
@@ -15,6 +15,7 @@ import Splitter, { SplitDirection } from '@devbookhq/splitter';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { generateRandomArray } from './lib/utils';
+import Header from './components/Header';
 
 const stateFields = { history: historyField };
 const editorStateKey = "editorState";
@@ -189,31 +190,7 @@ export default function Editor({ path }: { path: string }) {
             onResizeFinished={(_, newSizes) => localStorage.setItem(splitterStateKey, newSizes.join(","))}
         >
             <div className="h-screen flex flex-col border-r bg-gray-900 border-gray-800">
-                <div className="flex h-14 items-center justify-between border-b px-4 border-gray-800">
-                    <div className="flex items-center gap-2">
-                        <CodeIcon className="h-5 w-5 mr-2 text-gray-400" />
-                        <span className="text-lg font-medium text-gray-50">
-                            Laravel Web Tinker
-                        </span>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        {loading ? (
-                            <span className="text-xs text-gray-400 font-mono">
-                                {(elapsedTime / 1000).toFixed(1)}s
-                            </span>
-                        ): (
-                            <Button
-                                className="h-8 w-8 hover:bg-gray-800 text-gray-400 hover:text-gray-50"
-                                size="icon"
-                                variant="ghost"
-                                onClick={sendCurrentCode}
-                            >
-                                <PlayIcon className="h-4 w-4" />
-                                <span className="sr-only">Run</span>
-                            </Button>
-                        )}
-                    </div>
-                </div>
+                <Header loading={loading} onRun={sendCurrentCode} />
                 <div className="border-b border-gray-800 flex justify-between">
                     <div>
                         {tabs.map((tab: number) =>
